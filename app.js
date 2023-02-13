ad3e24903a9980b8785e4df4f0377480ab0ca6b5
Akatsuki-kun123 <tm09102003@gmail.com>
2023-02-13 16:15:16 +0700

    Done

---
+++ app.js
@@ -0,0 +1,132 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Tb15092003!",
+    database: "testdb"
+});
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+connection.connect(function(err) {
+    (err) ? console.log(err) : console.log(connection);
+});
+
+app.get('/api/users', (req, res) => {
+    var sql = "SELECT users.*, role_user.role_id, roles.name AS role FROM users, roles, role_user WHERE users.id=role_user.user_id AND role_user.role_id=roles.id";
+    connection.query(sql, function(err, results) {
+        if (err) throw err;
+        res.json({users: results});
+    });
+});
+
+app.post('/api/insert', function(req, res) {
+    var sql = "INSERT "
+            + "INTO users(name, password, email, ten, sdt, cmnd, ngaysinh, thangsinh, namsinh, khoi_id, cap1, cap2) "
+            + "VALUES('"
+            + req.body.name + "','" 
+            + req.body.password + "','" 
+            + req.body.email + "','"
+            + "' '" + "','"
+            + "' '" + "','"
+            + "' '" + "','"
+            + "0" + "','"
+            + 0 + "','"
+            + 0 + "','"
+            + 0 + "','"
+            + 0 + "','"
+            + 0 + "')";
+    connection.query(sql, function (err, results) {
+        if(err) throw err;
+        sql = "SELECT * FROM roles";
+        connection.query(sql, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            results.some((value, index) => {
+                if (req.body.role == value.name) {
+                    var userID = 0;
+                    var roleID = value.id;
+
+                    sql = "SELECT id, name FROM users";
+                    connection.query(sql, function (err, results) {
+                        if (err) {
+                            throw err;
+                        }
+                        results.some((value, index) => {
+                            if (req.body.name == value.name) {
+                                userID = value.id;
+                                sql = "INSERT "
+                                    + "INTO role_user(user_id, role_id) "
+                                    + "VALUES('"
+                                    + userID + "','" 
+                                    + roleID + "')";
+                                connection.query(sql, function (err, results) {
+                                    if(err) throw err;
+                                    res.json({users: results});
+                                });
+                            }
+                        });
+                    });
+                }
+            });
+        });
+    });
+});
+
+app.post('/api/edit', (req, res) => {
+    var sql = "UPDATE users SET "
+            + "name='" + req.body.name + "',"
+            + "password='" + req.body.password + "',"
+            + "email='" + req.body.email + "'"
+            + "WHERE id='" + req.body.id + "'";
+    connection.query(sql, function(err, results) {
+        if (err) throw err;
+        sql = "SELECT * FROM roles";
+        connection.query(sql, function (err, results) {
+            if (err) {
+                throw err;
+            }
+            results.some((value, index) => {
+                if (req.body.role == value.name) {
+                    console.log(value.name + value.id);
+                    sql = "UPDATE role_user SET "
+                        + "role_id='" + value.id + "'"
+                        + "WHERE user_id='" + req.body.id + "'";
+                    connection.query(sql, function (err, results) {
+                        if (err) {
+                            throw err;
+                        }
+                        res.json({users: results});
+                    });
+                }
+            });
+        });
+    });
+});
+
+app.post('/api/delete', (req, res) => {
+    var sql = "DELETE FROM users "
+            + "WHERE id='" + req.body.id + "'";
+    connection.query(sql, function(err, results) {
+        if (err) throw err;
+        sql = "DELETE FROM role_user "
+            + "WHERE user_id='" + req.body.id + "'";
+        connection.query(sql, function(err, results) {
+            if (err) {
+                throw err;
+            }
+            res.json({users: results});
+        })
+    });
+});
+
+app.listen(4000, () => console.log('App listening on port 4000'));
---
+++ testdb/src/App.js
@@ -0,0 +1,247 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import Modal from 'react-modal';
+
+class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+      users: [],
+      id: '',
+      name: '',
+      password: '',
+      email: '',
+      role: ''
+    }
+  };
+
+  componentDidMount() {
+      axios.get('/api/users')
+        .then(res => {
+          const users = res.data.users;
+          this.setState({users: users});
+        })
+        .catch(error => console.log(error));
+  };
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    console.log(name, value);
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  };
+
+  openModal = (user) => {
+    this.setState({
+      modalIsOpen: true,
+      id: user.id,
+      name: user.name,
+      password: user.password,
+      email: user.email,
+      role: user.role
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalIsOpen: false
+    });
+  };
+
+  handleEditSubmit = (event) => {
+    event.preventDefault();
+
+    const userUpdate = {
+      id: this.state.id,
+      name: this.state.name,
+      password: this.state.password,
+      email: this.state.email,
+      role: this.state.role
+    };
+
+    axios.post('/api/edit', userUpdate)
+      .then(res => {
+        let key = this.state.id;
+        this.setState(prevState => ({
+          users: prevState.users.map(
+            elem => elem.id === key? {
+              ...elem,
+              name: this.state.name,
+              password: this.state.password,
+              email: this.state.email,
+              role: this.state.role
+            }: elem
+          )
+        }))
+      })
+      .catch(error => console.log(error));
+  };
+  
+  handleInsertSubmit = (event) => {
+    event.preventDefault();
+  
+    const newUser = {
+      id: '',
+      name: this.state.name,
+      password: this.state.password,
+      email: this.state.email,
+      role: this.state.role
+    };
+
+    axios.post('/api/insert', newUser)
+      .then(res => {
+        let users = this.state.users;
+        users = [newUser,...users];
+        this.setState({users: users});
+      })
+      .catch(error => console.log(error));
+  };
+
+  handleDelete = (user) => {
+    const newsId = {
+      id: user.id
+    };
+    
+    axios.post('/api/delete', newsId)
+      .then(res => {
+        this.setState(prevState => ({
+          users: prevState.users.filter(elem => elem.id !== user.id)
+        }));
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Add an user</h2>
+        <form onSubmit={this.handleInsertSubmit}>
+          <table>
+            <tbody>
+              <tr>
+                <th><label>User's name</label></th>
+                <td>
+                  <input
+                    name="name"
+                    type="text"
+                    onChange={this.handleInputChange} />
+                </td>
+              </tr>
+
+              <tr>
+                <th><label>Password</label></th>
+                <td>
+                  <textarea
+                    name="password"
+                    onChange={this.handleInputChange} />
+                </td>
+              </tr>
+
+              <tr>
+                <th><label>Email</label></th>
+                <td>
+                  <textarea
+                    name="email"
+                    onChange={this.handleInputChange} />
+                </td>
+              </tr>
+
+              <tr>
+                <th><label>Role</label></th>
+                <td>
+                  <select name='role' onChange={this.handleInputChange}>
+                    <option value="none">(none)</option>
+                    <option value="admin">Admin</option>
+                    <option value="guest">Guest</option>
+                  </select>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+          <button type="submit">Submit</button>
+        </form>
+
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>
+              <h2>{user.name} ({user.role})</h2>
+              <div>{user.email}</div>
+              <button onClick={() => this.openModal(user)}>Edit</button>
+              <button onClick={() => this.handleDelete(user)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}>
+          <button onClick={this.closeModal}>Close</button>
+          
+          <form onSubmit={this.handleEditSubmit}>
+            <table>
+              <tbody>
+                <tr>
+                  <th><label>Name</label></th>
+                  <td>
+                    <input
+                      type="text"
+                      name="name"
+                      value={this.state.name} 
+                      onChange={this.handleInputChange} />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th><label>Password</label></th>
+                  <td>
+                    <textarea
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleInputChange} />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th><label>Email</label></th>
+                  <td>
+                    <textarea
+                      name="email"
+                      value={this.state.email} 
+                      onChange={this.handleInputChange} />
+                  </td>
+                </tr>
+
+                <tr>
+                  <th><label>Role</label></th>
+                  <td>
+                    <select name="role" onChange={this.handleInputChange} value={this.state.role}>
+                      <option value="none">(none)</option>
+                      <option value="admin">Admin</option>
+                      <option value="guest">Guest</option>
+                    </select>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <button type="submit">Edit</button>
+          </form>
+        </Modal>
+      </div>
+    )
+  };
+};
+
+export default App;
